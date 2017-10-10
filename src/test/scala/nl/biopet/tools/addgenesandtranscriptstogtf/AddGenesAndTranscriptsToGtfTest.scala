44be64698989139ca5150678600d8878f82d15bd
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtfTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.addgenesandtranscriptstogtf
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object AddGenesAndTranscriptsToGtfTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
