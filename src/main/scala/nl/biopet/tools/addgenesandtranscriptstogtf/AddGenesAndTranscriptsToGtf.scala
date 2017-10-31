b738206198d44a4493550b04e0f703c6632e323e
pjvan_thof <pjrvanthof@gmail.com>
2017-10-31 15:21:05 +0100

    Replace to new tool testing

--- src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtf.scala
+++ src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtf.scala
@@ -8,7 +8,7 @@ import nl.biopet.utils.tool.ToolCommand
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-object AddGenesAndTranscriptsToGtf extends ToolCommand {
+object AddGenesAndTranscriptsToGtf extends ToolCommand[Args] {
   def main(args: Array[String]): Unit = {
     val parser = new ArgsParser(toolName)
     val cmdArgs =
