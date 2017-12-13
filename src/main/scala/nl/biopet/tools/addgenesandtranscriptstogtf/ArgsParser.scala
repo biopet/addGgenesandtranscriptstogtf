b2d9aa13a030a386afb7c31aeabb81ce3ac8bad6
DavyCats <davycats.dc@gmail.com>
2017-12-13 16:17:10 +0100

    remove unbounded ()

--- src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/ArgsParser.scala
@@ -5,11 +5,11 @@ import java.io.File
 import nl.biopet.utils.tool.AbstractOptParser
 
 class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
-  opt[File]('I', "input") required () unbounded () valueName "<file>" action {
+  opt[File]('I', "input") required () valueName "<file>" action {
     (x, c) =>
       c.copy(input = x)
   } text "Input gtf file. Mandatory"
-  opt[File]('o', "output") required () unbounded () valueName "<file>" action {
+  opt[File]('o', "output") required () valueName "<file>" action {
     (x, c) =>
       c.copy(output = x)
   } text "Output gtf file. Mandatory"
