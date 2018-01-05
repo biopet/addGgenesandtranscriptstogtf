7d049d2d26ed6a7ec95249ae0fab57d1150b5c5c
rhpvorderman <r.h.p.vorderman@lumc.nl>
2018-01-05 10:39:41 +0100

    generated headers

--- src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtf.scala
+++ src/main/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtf.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.addgenesandtranscriptstogtf
 
 import java.io.PrintWriter
@@ -94,14 +115,22 @@ object AddGenesAndTranscriptsToGtf extends ToolCommand[Args] {
   }
 
   def descriptionText: String =
-    """
-      |This tool adds genes and transcripts to a GTF file, based on the exons available in an input GTF file.
+    s"""
+      |
+      |This tool repairs a broken GTF file that does not contain all genes and transcripts.
+      |A proper GTF file lists genes, transcripts and exons. However some tools only add exons to the GTF file.
+      |$toolName adds genes and transcripts to a GTF file, based on the exons available in an input GTF file.
+      |
     """.stripMargin
 
-  def manualText: String = ???
+  def manualText: String =
+    s"""
+       |$toolName determines the genes and transcripts from the listed exons in the input GTF.
+       |Only an input and an output GTF need to be passed to the command line.
+     """.stripMargin
 
   def exampleText: String =
-    s"""
+    s"""To create a GTF file with genes and transcripts from a GTF file with missing genes and transcripts:
        |${example("-I", "input.gtf", "-o", "output.gtf")}
      """.stripMargin
 }
