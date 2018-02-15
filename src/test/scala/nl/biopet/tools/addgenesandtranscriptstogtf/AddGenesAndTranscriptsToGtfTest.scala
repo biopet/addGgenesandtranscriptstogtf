609035bb0f3a7fd59d6ac57f6effac7027b25f0d
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtfTest.scala
+++ src/test/scala/nl/biopet/tools/addgenesandtranscriptstogtf/AddGenesAndTranscriptsToGtfTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.addgenesandtranscriptstogtf
 
 import java.io.File
@@ -17,15 +38,25 @@ class AddGenesAndTranscriptsToGtfTest extends ToolTest[Args] {
     }
   }
 
-  def toolCommand: AddGenesAndTranscriptsToGtf.type = AddGenesAndTranscriptsToGtf
+  def toolCommand: AddGenesAndTranscriptsToGtf.type =
+    AddGenesAndTranscriptsToGtf
 
   @Test
   def testMain(): Unit = {
     val inputFile = File.createTempFile("test.", ".gtf")
     inputFile.deleteOnExit()
     val gene1 =
-      Feature("chrQ", "test", "gene", 11, 40, None, Some(true), None, Map("gene_id" -> "gene_1"))
-    val transcript1 = Feature("chrQ",
+      Feature("chrQ",
+              "test",
+              "gene",
+              11,
+              40,
+              None,
+              Some(true),
+              None,
+              Map("gene_id" -> "gene_1"))
+    val transcript1 = Feature(
+      "chrQ",
       "test",
       "transcript",
       11,
@@ -34,7 +65,8 @@ class AddGenesAndTranscriptsToGtfTest extends ToolTest[Args] {
       Some(true),
       None,
       Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
-    val exon1 = Feature("chrQ",
+    val exon1 = Feature(
+      "chrQ",
       "test",
       "exon",
       11,
@@ -43,7 +75,8 @@ class AddGenesAndTranscriptsToGtfTest extends ToolTest[Args] {
       Some(true),
       None,
       Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
-    val exon2 = Feature("chrQ",
+    val exon2 = Feature(
+      "chrQ",
       "test",
       "exon",
       31,
